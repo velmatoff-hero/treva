1c9c07b559768943a6d62978d0a4caa929e9ec75
Aleksey Velmatov <velmatovaleksey@fromzerotowebhero.ru>
2020-10-07 13:04:29 +0300

    create appBar with gradient and add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+import 'src/components/GradientAppBar.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Planets", home: HomePage()));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [GradientAppBar('treva')],
+      ),
+    );
+  }
+}
